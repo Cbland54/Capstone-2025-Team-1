67f72acd3aab4921b308114327f1416c8c499629
icpms54 <connorbland54@outlook.com>
2025-10-15 17:39:25 -0400

    local variables

--- src/components/supabaseClient.jsx
+++ src/components/supabaseClient.jsx
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://mbiccyyifphapqvakwwe.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iaWNjeXlpZnBoYXBxdmFrd3dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzMzUxMTksImV4cCI6MjA3NDkxMTExOX0.vK4tCgYlSI-3b6ZTQgl06c9APuhCSTPnpYQ6XUMRpVE";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
