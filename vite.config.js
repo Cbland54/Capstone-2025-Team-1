371899af60bff26fa0950ddf9f700f3214656421
icpms54 <connorbland54@outlook.com>
2025-10-27 15:10:45 -0400

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwind from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Capstone-2025-Team-1/',
+  plugins: [
+    react(),
+    tailwindcss(), // 
+  ],
+  base: '/Capstone-2025-Team-1/', // 
 })
